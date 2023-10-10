e897529f2b14529195b794f23aba12fc012017e8
Michael <michaelarnold615@gmail.com>
2023-10-10 14:27:46 -0700

    Removed all testing code and restored back
    Found the error -> it was in curl_init()

--- contact-submit.php
+++ contact-submit.php
@@ -1,9 +1,5 @@
 <?php
 
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
-
 // Import PHPMailer classes into the global namespace
 // These must be at the top of your script, not inside a function
 use PHPMailer\PHPMailer\PHPMailer;
@@ -17,29 +13,29 @@ require 'vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-// START FORM PROCESS
-// Check if form is submitted
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-    // SET RECAPTCHA
-    $recaptcha = $_POST['g-recaptcha-response'];
-    $res = reCaptcha($recaptcha);
-
-    if ($res['success']) {
-        echo '<script>alert("CORRECT: ReCaptcha Successful")</script>';
-    }else{
-        echo '<script>alert("ERROR: ReCaptcha Not Successful")</script>';
-    }
+// Clean user input function
+function clean_input($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
 }
 
+// Reset Form
+function reset_form_data()
+{
+    $name = $email = $message = "";
+    $name_err = $email_err = $message_err = "";
+}
 
 // RECAPTCHA
-function reCaptcha($recaptcha){
-    $secret = $_ENV["SECRET_KEY"];
+function reCaptcha($recaptcha)
+{
+    $secret = $_ENV['SECRET_KEY'];
     $ip = $_SERVER['REMOTE_ADDR'];
-  
-    $postvars = array("secret"=>$secret, "response"=>$recaptcha, "remoteip"=>$ip);
+
+    $postvars = array("secret" => $secret, "response" => $recaptcha, "remoteip" => $ip);
     $url = "https://www.google.com/recaptcha/api/siteverify";
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
@@ -48,8 +44,99 @@ function reCaptcha($recaptcha){
     curl_setopt($ch, CURLOPT_POSTFIELDS, $postvars);
     $data = curl_exec($ch);
     curl_close($ch);
-  
     return json_decode($data, true);
 }
 
-?>
+// Set initial variables to empty string ""
+$name = $email = $message = "";
+$name_err = $email_err = $message_err = "";
+
+
+// START FORM PROCESS
+// Check if form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    // Check required fields are filled
+    if (!isset($_POST["name"]) || !isset($_POST["email"]) || !isset($_POST["message"])) {
+        echo "Sorry one of the required fields is missing, please try again!";
+        $name_err = $email_err = $message_err = "REQUIRED FIELDS";
+        die();
+    }
+
+    // NAME
+    $name = clean_input($_POST["name"]);
+    if (!preg_match("/^[a-zA-Z ]*$/", $name)) {
+        $name_err = "Only letters and whitespace allowed.";
+    }
+
+    // EMAIL
+    $email = clean_input($_POST["email"]);
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $email_error = "Invalid email format";
+    }
+
+    // MESSAGE
+    $message = clean_input($_POST["message"]);
+
+    // SET RECAPTCHA
+    $recaptcha = $_POST['g-recaptcha-response'];
+    $res = reCaptcha($recaptcha);
+
+    if ($res['success']) {
+        if ($name_err == "" && $email_err == "" && $message_err == "") {
+            // SEND EMAIL
+    
+            //Unset the post submission (for next load)
+            unset($_POST['submit']);
+    
+            // Compose the email
+            $composed_email = "";
+            $composed_email .= "Name: " . $name . "<br>";
+            $composed_email .= "Email: " . $email . "<br>";
+            $composed_email .= "Message: " . $message . "<br>";
+    
+            // Create new PHPMailer object
+            $mail = new PHPMailer(TRUE);
+    
+            // Test Settings for SMTP Server
+            $mail->SMTPDebug = SMTP::DEBUG_OFF;
+            $mail->isSMTP();
+            $mail->Host       = $_ENV["SMTP_HOST"];
+            $mail->SMTPAuth   = true;
+            $mail->Username   = $_ENV["FROM_EMAIL"];
+            $mail->Password   = $_ENV["FROM_EMAIL_PASS"];
+            $mail->Port       = 587;
+            $mail->SMTPOptions = array(
+                'ssl' => array(
+                    'verify_peer' => false,
+                    'verify_peer_name' => false,
+                    'allow_self_signed' => true
+                )
+            );
+    
+            //Recipients
+            $mail->setFrom($_ENV["FROM_EMAIL"], 'Inland Empire Website');           // Set default address that emails are sent from
+            $mail->addAddress($_ENV["TO_EMAIL"], 'Inland Empire Drywall');          // This is who the email is being sent to
+    
+            // Content
+            $mail->isHTML(true);                                              // Set email format to HTML
+            $mail->Subject = 'Request More Information From Website';         // Subject
+            $mail->Body    = $composed_email;                                 // Body
+            $mail->AltBody = $composed_email;                                 // Alt. Body
+    
+            // Send the email
+            if ($mail->send()) {
+                $mailsend_success = "Message sent! Thank you for contacting us.";
+                // echo '<script>alert("' . $mailsend_success . '")</script>';
+                reset_form_data();
+                header("Location: thankyou.html");
+            } else {
+                $mailsend_error = "Message could not be sent. Mailer Error: {" . $mail->ErrorInfo . "}";
+                echo '<script>alert("' . $mailsend_error . '")</script>';
+                reset_form_data();
+            }
+        }
+    }else {
+        echo '<script>alert("ERROR: ReCaptcha Not Successful")</script>';
+    }
+}
